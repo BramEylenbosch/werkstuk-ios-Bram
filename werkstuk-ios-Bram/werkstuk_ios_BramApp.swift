58e72c3c9dbdd155fe56cec83377f2c920266ac4
BramEylenbosch <bram.eylenbosch@student.ehb.be>
2024-04-19 16:16:50 +0200

    Initial Commit

---
+++ werkstuk-ios-Bram/werkstuk_ios_BramApp.swift
@@ -0,0 +1,17 @@
+//
+//  werkstuk_ios_BramApp.swift
+//  werkstuk-ios-Bram
+//
+//  Created by ehb on 19/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct werkstuk_ios_BramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
